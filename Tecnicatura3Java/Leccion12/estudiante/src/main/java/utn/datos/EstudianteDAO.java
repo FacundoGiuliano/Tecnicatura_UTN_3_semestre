3a004df2af7db2358d58bdc9e9aec6863e2cd1de
Facundo Giuliano <facugiuliano10@gmail.com>
2023-07-15 20:38:02 -0300

    Terminada clase 12

--- Tecnicatura3Java/Leccion12/estudiante/src/main/java/utn/datos/EstudianteDAO.java
+++ Tecnicatura3Java/Leccion12/estudiante/src/main/java/utn/datos/EstudianteDAO.java
@@ -9,7 +9,7 @@ import java.util.List;
 import utn.dominio.Estudiante;
 
 public class EstudianteDAO {
-    public List<Estudiante> listar(){
+    public List<Estudiante> listarEstudiantes(){
         List<Estudiante> estudiantes = new ArrayList<>();
         // Objetos necesarios para comunicarnos con la bd
         PreparedStatement ps; // Envia la sentencia
@@ -40,5 +40,121 @@ public class EstudianteDAO {
             }
         }
         return estudiantes;
+    }
+
+    // Metodo agregar un nuevo estudiante
+    public boolean agregarEstudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "INSERT INTO estudiantes2022 (nombre, apellido, telefono, email)VALUES (?, ?, ?, ?)";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, estudiante.getNombre());
+            ps. setString(2, estudiante.getApellido());
+            ps.setString(3, estudiante.getTelefono());
+            ps.setString(4, estudiante.getEmail());
+            ps.execute();
+            return true;
+        }catch(Exception e){
+            System.out.println("Ocurrio un error al agregar el estudiante: "+e.getMessage());
+        }
+        finally{
+            try{
+                con.close();
+            } catch (Exception e){
+                System.out.println("Error al cerrar la conexion: "+e.getMessage());
+            }
+        }
+        return false;
+    }
+
+        // Modificar estudiante
+    public boolean modificarEstudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "UPDATE estudiantes2022 SET nombre=?, apellido=?, telefono=?, email=? WHERE idestudiantes2022=?";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, estudiante.getNombre());
+            ps.setString(2, estudiante.getApellido());
+            ps.setString(3, estudiante.getTelefono());
+            ps.setString(4, estudiante.getEmail());
+            ps.setInt(5, estudiante.getIdEstudiante());
+            ps.execute();
+            return true;
+        } catch (Exception e){
+            System.out.println("Error al modificar estudiante: "+e.getMessage());
+        }
+        finally{
+            try{
+                con.close();
+            } catch (Exception e){
+                System.out.println("Error al cerrar la conexion: "+e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    // Metodo por id -> find by id
+    public boolean buscarEstudiantePorId(Estudiante estudiante){
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiantes2022 WHERE idestudiantes2022=?";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            rs = ps.executeQuery();
+            if(rs.next()){
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                return true; // Se encontro un registro
+            }
+        } catch (Exception e){
+            System.out.println("Ocurrio un error al buscar estudiante: "+e.getMessage());
+        }
+        finally{
+            try{
+                con.close();
+            } catch (Exception e){
+                System.out.println("Ocurrio un error al cerrar la conexion: "+e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        var estudianteDAO = new EstudianteDAO();
+        // Modificar estudiante
+        var estudianteModificado = new Estudiante(1, "Juan Carlos", "Juarez", "356854", "juan@mail.com");
+        var modificado = estudianteDAO.modificarEstudiante(estudianteModificado);
+        if(modificado)
+            System.out.println("Estudiante modificado: "+estudianteModificado);
+        else
+            System.out.println("No se modifico el estudiante: "+estudianteModificado);
+
+        // Agregar estudiante
+        /*var nuevoEstudiante = new Estudiante("Carlos", "Lara", "2345674", "clara@mail.com");
+        var agregado = estudianteDAO.agregarEstudiante(nuevoEstudiante);
+        if(agregado)
+            System.out.println("Estudiante agregado: "+nuevoEstudiante);
+        else
+            System.out.println("No se ha agregado estudiantes: "+nuevoEstudiante);*/
+
+        // Listar los estudiantes
+        System.out.println("Listado de estudiantes: ");
+        List<Estudiante> estudiantes = estudianteDAO.listarEstudiantes();
+        estudiantes.forEach(System.out::println); // Funcion lambda para imprimir
+
+        // Buscar por id
+        /*var estudiante1 = new Estudiante(1);
+        System.out.println("Estudiantes antes de la busqueda: "+estudiante1);
+        var encontrado = estudianteDAO.buscarEstudiantePorId(estudiante1);
+        if(encontrado)
+            System.out.println("Estudiante encontrado: "+estudiante1);
+        else
+            System.out.println("No se encontro el estudiante: "+estudiante1.getIdEstudiante());*/
     }
 }
